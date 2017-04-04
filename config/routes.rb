e7405f5f912e8ffa3a15e4a35d45b8c33df2718a
Adam <adam.mandeville@gmail.com>
2017-04-04 15:47:29 -0400

    added venue pics

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'party' => 'welcome#party', as: "party"
   get 'itinerary' => 'welcome#itinerary', as: "itinerary"
   get 'activities' => 'welcome#activities', as: "activities"
+  get 'venue' => 'welcome#venue', as: "venue"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
