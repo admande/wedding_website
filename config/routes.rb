bd08f5bd7f34a404e2ae771e187617af7002779f
Adam <adam.mandeville@gmail.com>
2017-04-04 11:59:57 -0400

    added activities and a hotel

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'registry' => 'welcome#registry', as: "registry"
   get 'party' => 'welcome#party', as: "party"
   get 'itinerary' => 'welcome#itinerary', as: "itinerary"
+  get 'activities' => 'welcome#activities', as: "activities"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
